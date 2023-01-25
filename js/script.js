2f5b0ba0fa292661ba2acafb5510040904839213
srikanth-kandi <i.am.srikanth.kandi@gmail.com>
2023-01-25 19:39:29 +0530

    Changed the year for footer section using JS

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.querySelector('#search-btn').onclick = () => {
 
 let navbar = document.querySelector('.header .navbar');
 
-document.querySelector('#menu-btn').onclick = () =>{
+document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
 }
@@ -22,13 +22,13 @@ window.onscroll = () => {
 // manual slides
 let slides = document.querySelectorAll('.home .slide');
 let index = 0;
-function next(){
+function next() {
     slides[index].classList.remove('active');
     index = (index + 1) % slides.length;
     slides[index].classList.add('active');
 }
 
-function prev(){
+function prev() {
     slides[index].classList.remove('active');
     index = (index - 1 + slides.length) % slides.length;
     slides[index].classList.add('active');
@@ -39,15 +39,15 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
+        delay: 7500,
+        disableOnInteraction: false,
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    loop:true,
-  });
+    loop: true,
+});
 
 
 // currency toggler
@@ -59,45 +59,44 @@ let usd = 74.96; // as of 31-01-2022: 1$ = ₹74.96
 
 toggler.onclick = () => {
     toggler.classList.toggle('active');
-    if(toggler.classList.contains('active')){
-        for(var i=0;i < dollar.length; i++){
+    if (toggler.classList.contains('active')) {
+        for (var i = 0; i < dollar.length; i++) {
             dollar[i].innerText = '₹';
         }
-        for(var k=0;k < price.length; k++){
+        for (var k = 0; k < price.length; k++) {
             price[k].innerText = (price[k].innerText * usd).toFixed(2);
         }
     }
-    else{
-        for(var i=0;i < dollar.length; i++){
+    else {
+        for (var i = 0; i < dollar.length; i++) {
             dollar[i].innerText = '$';
         }
-        for(var k=0;k < price.length; k++){
+        for (var k = 0; k < price.length; k++) {
             price[k].innerText = (price[k].innerText / usd).toFixed(2);
         }
     }
 }
 
 // Login validation
-function readValues()
-        {
-            // read name from the form and store in the variable
-            // variablename = formname.componentname.value; or
-            // variablename = document.formname.componentname.value;
-            username = login_form.name.value;
-            // read password from the form and store in the variable.
-            password = document.login_form.pwd.value;
-            // check the name field is empty or not
-            if(username =="" || username == null){
-                alert("Please enter username")
-            }
-            else if(password == "" || password == null){
-                alert("Please enter password")
-            }
-            else if(password.length < 8){
-                alert("Please enter 8 digit passowrd ")
-            }
-            else{
-                // successfull login
-                window.open('index.html','_blank');
-            }
-        }
+function readValues() {
+    // read name from the form and store in the variable
+    // variablename = formname.componentname.value; or
+    // variablename = document.formname.componentname.value;
+    username = login_form.name.value;
+    // read password from the form and store in the variable.
+    password = document.login_form.pwd.value;
+    // check the name field is empty or not
+    if (username == "" || username == null) {
+        alert("Please enter username")
+    }
+    else if (password == "" || password == null) {
+        alert("Please enter password")
+    }
+    else if (password.length < 8) {
+        alert("Please enter 8 digit passowrd ")
+    }
+    else {
+        // successfull login
+        window.open('index.html', '_blank');
+    }
+}
